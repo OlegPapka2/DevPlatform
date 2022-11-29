dae1da61d54df5dca29beb527ff669cb5f5fbaa2
Oleg Papka <olegpapka2@gmail.com>
2022-11-29 16:41:11 +0200

    Added photo to Profile page, changed style of nav bar

--- src/profile/views.py
+++ src/profile/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, url_for
+from flask_login import login_required, current_user
 
 profile_bp = Blueprint(
     'profile', __name__,
@@ -12,4 +12,12 @@ profile_bp = Blueprint(
 @login_required
 def profile():
     """User profile page"""
-    return render_template('profile.html')
+
+    profile_picture = current_user.picture
+
+    if profile_picture:
+        picture_path = url_for('static', filename=f'profile_pictures/{profile_picture}')
+    else:
+        picture_path = url_for('static', filename='profile_pictures/default_profile_picture.png')
+
+    return render_template('profile.html', profile_picture=picture_path)
